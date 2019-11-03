3ffd34fbac2e289f1afa79cafdba7939d11dbcbe
kkyeongin <kkyeongin@gmail.com>
2019-11-03 17:11:13 +0900

    gatsby-meta-config modify

---
+++ gatsby-meta-config copy.js
@@ -0,0 +1,29 @@
+module.exports = {
+  title: `In Progress`,
+  description: `hello world`,
+  author: `Kyeongin Kim`,
+  introduction: `It is a blog that will record my development records.`,
+  siteUrl: 'kkyeongin.github.io', // Your blog site url
+  social: {
+    twitter: ``, // Your Twitter account
+    github: `kkyeongin`, // Your GitHub account
+    medium: ``, // Your Medium account
+    facebook: ``, // Your Facebook account
+  },
+  icon: `content/assets/icon.png`, // Add your favicon
+  keywords: [`blog`],
+  comment: {
+    disqusShortName: 'kkyeongin.github.io', // Your disqus-short-name. check disqus.com.
+    utterances: 'kkyeongin/kkyeongin.github.io', // Your repository for archive comment
+  },
+  configs: {
+    countOfInitialPost: 10, // Config your initial count of post
+  },
+  sponsor: {
+    buyMeACoffeeId: 'QRsTu4dlJ',
+  },
+  share: {
+    facebookAppId: '', // Add facebookAppId for using facebook share feature v3.2
+  },
+  ga: 'kkyeogatsby-meta-configngin', // Add your google analytics tranking ID
+}
